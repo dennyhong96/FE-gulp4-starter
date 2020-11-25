7e24228a3dfa4e7f834a29ddbe687693fb9eba32
Haiyang Hong <hong961127@gmail.com>
2020-11-24 23:30:53 -0800

    Add watch function to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const browserSync = require("browser-sync").create();
 
-// Compile scss into css
+// Compiles scss into css
 function style() {
   return (
     gulp
@@ -12,7 +12,31 @@ function style() {
       .pipe(sass())
       // Saves compiled CSS
       .pipe(gulp.dest("./css"))
+      // Streams changes to all browsers (w/o refresh)
+      .pipe(browserSync.stream())
   );
 }
 
-exports.style = style;
+// Watches for file changes
+function watch() {
+  // Creates a dev server
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+
+  // Re-compile scss upon changes
+  gulp.watch("./src/scss/**/*.scss", style);
+
+  // Refresh browser when HTML changes
+  gulp.watch("./**/*.html").on("change", browserSync.reload);
+
+  // Refresh browser when JavaScript changes
+  gulp.watch("./src/js/**/*.js").on("change", browserSync.reload);
+}
+
+module.exports = {
+  style,
+  watch,
+};
